419cfafd2169f10208c80f515d26b3fe8257efc3
nuranajs <nurana.co@gmail.com>
2024-11-19 13:26:00 +0700

    fix layout

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,11 @@ import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
-import { ThemeProvider } from "./components/context/ThemeContext";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <ThemeProvider>
+      
       
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
@@ -21,7 +20,6 @@ export default function Home() {
         <EmailSection />
       </div>
       <Footer />
-      </ThemeProvider>
     </main>
   );
 }
